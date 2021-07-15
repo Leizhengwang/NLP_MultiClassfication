e77048be3265b72c522fe456f9a19ea6f104d213
Leizhengwang <58499615+Leizhengwang@users.noreply.github.com>
2021-07-14 19:07:50 -0500

    Update Main.py

--- Main.py
+++ Main.py
@@ -50,5 +50,5 @@ def preprocessDataAndPredict(Job_description):
     return prediction
     pass
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='127.0.0.1', port=5000)
 
